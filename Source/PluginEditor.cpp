00aa2cea2fde406556d1f4c4f9a7bd9101b4df89
ddx64 <102793027+ddx64@users.noreply.github.com>
2025-12-02 17:21:04 +0800

    Midi keyboard test passed

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -10,21 +10,25 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-MusicoAudioProcessorEditor::MusicoAudioProcessorEditor (MusicoAudioProcessor& p)
-    : AudioProcessorEditor (&p),
-     #if JucePlugin_Enable_ARA
-      AudioProcessorEditorARAExtension (&p),
-     #endif
-      audioProcessor (p)
+MusicoAudioProcessorEditor::MusicoAudioProcessorEditor(MusicoAudioProcessor& p)
+	: AudioProcessorEditor(&p),
+#if JucePlugin_Enable_ARA
+	AudioProcessorEditorARAExtension(&p),
+#endif
+	audioProcessor(p), midiKeyboard(audioProcessor.getKeyboardState(), juce::MidiKeyboardComponent::horizontalKeyboard)
 {
-   #if JucePlugin_Enable_ARA
-    // ARA plugins must be resizable for proper view embedding
-    setResizable (true, false);
-   #endif
+#if JucePlugin_Enable_ARA
+	// ARA plugins must be resizable for proper view embedding
+	setResizable(true, false);
+#endif
 
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+	// Key range A0-C8
+	midiKeyboard.setAvailableRange(21, 108);
+	addAndMakeVisible(midiKeyboard);
+
+	// Make sure that before the constructor has finished, you've set the
+	// editor's size to whatever you need it to be.
+	setSize(1200, 200);
 }
 
 MusicoAudioProcessorEditor::~MusicoAudioProcessorEditor()
@@ -32,18 +36,19 @@ MusicoAudioProcessorEditor::~MusicoAudioProcessorEditor()
 }
 
 //==============================================================================
-void MusicoAudioProcessorEditor::paint (juce::Graphics& g)
+void MusicoAudioProcessorEditor::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
+	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+	g.setColour(juce::Colours::white);
+	g.setFont(juce::FontOptions(15.0f));
+	g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void MusicoAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+	// This is generally where you'll want to lay out the positions of any
+	// subcomponents in your editor..
+	midiKeyboard.setBounds(getLocalBounds());
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -14,23 +14,29 @@
 //==============================================================================
 /**
 */
-class MusicoAudioProcessorEditor  : public juce::AudioProcessorEditor
-                            #if JucePlugin_Enable_ARA
-                             , public juce::AudioProcessorEditorARAExtension
-                            #endif
+class MusicoAudioProcessorEditor : public juce::AudioProcessorEditor
+#if JucePlugin_Enable_ARA
+	, public juce::AudioProcessorEditorARAExtension
+#endif
 {
 public:
-    MusicoAudioProcessorEditor (MusicoAudioProcessor&);
-    ~MusicoAudioProcessorEditor() override;
+	MusicoAudioProcessorEditor(MusicoAudioProcessor&);
+	~MusicoAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    MusicoAudioProcessor& audioProcessor;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	MusicoAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MusicoAudioProcessorEditor)
+	/*********************************************************************************
+	* Add functions and variables here
+	*********************************************************************************/
+	juce::MidiKeyboardComponent midiKeyboard;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicoAudioProcessorEditor)
+
 };
